d10e0620fa49587a9b0b4e6537dd9aa548994187
Pevnak <pevnak@gmail.com>
2019-06-28 05:46:09 +0200

    first version of gradient calculation of basic mul works

--- src/Unitary.jl
+++ src/Unitary.jl
@@ -7,6 +7,7 @@ struct UnitaryMatrix{T}
 	θ::T	
 end
 
+Flux.param(a::UnitaryMatrix) = UnitaryMatrix(param(a.θ))
 Flux.@treelike(UnitaryMatrix)
 
 
@@ -28,25 +29,27 @@ Flux.data(a::UnitaryMatrix) = UnitaryMatrix(Flux.data(a.θ))
 
 LinearAlgebra.transpose(a::UnitaryMatrix) = LinearAlgebra.Transpose(a)
 
-*(a::UnitaryMatrix{T}, x) where {T<:Vector} = _mulax(x, a.θ)
-_mulax(x, θ) = _mulax(x , sin(θ[1]), cos(θ[1]))
+*(a::UnitaryMatrix, x) = _mulax(a.θ, x)
+_mulax(θ, x) = _mulax((sin(θ[1]), cos(θ[1])), x)
 
-*(a::Transpose{X,A}, x::AbstractArray{T,2}) where {X, A<:UnitaryMatrix, T} = _mulatx(x, a.parent.θ)
-_mulatx(x, θ) = _mulax(x , - sin(θ[1]), cos(θ[1]))
+*(a::Transpose{X,A}, x::AbstractArray{T,2}) where {X, A<:UnitaryMatrix, T} = _mulatx(a.parent.θ, x)
+_mulatx(θ, x) = _mulax((- sin(θ[1]), cos(θ[1])), x)
 
-function _mulax(x, sinθ, cosθ)
+function _mulax(sincosθ::Tuple, x)
+	sinθ, cosθ = sincosθ
 	@assert size(x, 1) == 2
 	o = similar(x)
-	@inbounds for i in 1:size(x, 2)
+	for i in 1:size(x, 2)
 		o[1, i] =  cosθ * x[1,i] - sinθ * x[2,i]
 		o[2, i] =  sinθ * x[1,i] + cosθ * x[2,i]
 	end
 	o
 end
 
-_∇mulax(θ, Δ, x) = _∇mulax(sin(θ[1]), cos(θ[1]), Δ, x)
-function _∇mulax(sinθ, cosθ, Δ, x::AbstractArray{T,2}) where {P<:TrackedArray, T}
-	∇θ = similar(Flux.data(Δ), 1)
+_∇mulax(θ, Δ, x) = _∇mulax(Δ, (sin(θ[1]), cos(θ[1])), x)
+function _∇mulax(Δ, sincosθ::Tuple, x::AbstractArray{T,2}) where {P<:TrackedArray, T}
+	sinθ, cosθ = sincosθ
+	∇θ = similar(Δ, 1)
 	fill!(∇θ, 0)
 	for i in 1:size(x, 2)
 		∇θ[1] +=  Δ[1,i] * (- sinθ * x[1,i] - cosθ * x[2,i])
@@ -55,13 +58,14 @@ function _∇mulax(sinθ, cosθ, Δ, x::AbstractArray{T,2}) where {P<:TrackedArr
 	∇θ
 end
 
-*(x, a::UnitaryMatrix{T}) where {T<:Vector} = _mulxa(x, a.θ)
-_mulxa(x, θ) = _mulxa(x, sin(θ[1]), cos(θ[1]))
+*(x, a::UnitaryMatrix) = _mulxa(x, a.θ)
+_mulxa(x, θ) = _mulxa(x, (sin(θ[1]), cos(θ[1])))
 
 *(x::AbstractArray{T,2}, a::Transpose{X, A}) where {X, A<:UnitaryMatrix, T} = _mulxat(x, a.parent.θ)
-_mulxat(x, θ) = _mulxa(x, - sin(θ[1]), cos(θ[1]))
+_mulxat(x, θ) = _mulxa(x, (- sin(θ[1]), cos(θ[1])))
 
-function _mulxa(x, sinθ, cosθ)
+function _mulxa(x, sincosθ::Tuple)
+	sinθ, cosθ = sincosθ 
 	@assert size(x, 2) == 2
 	o = similar(x)
 	for i in 1:size(x, 1)
@@ -71,15 +75,17 @@ function _mulxa(x, sinθ, cosθ)
 	o
 end
 
-# *(a::UnitaryMatrix{T}, b::AbstractMatrix) where {T<: TrackedArray} = Flux.Tracker.track(*, a, b)
-# *(a::AbstractMatrix, b::UnitaryMatrix{T}) where {T<: TrackedArray} = Flux.Tracker.track(*, a, b)
-# Flux.Tracker.@grad function *(a::UnitaryMatrix{P}, b) where {P<:TrackedArray}
-#   return Flux.data(a) * b , Δ -> (Δ * transpose(b), transpose(a) * Δ)
-# end
+_mulax(a::TrackedArray, x::AbstractMatrix) = Flux.Tracker.track(_mulax, a, x)
+_mulatx(a::TrackedArray, x::AbstractMatrix) = Flux.Tracker.track(_mulatx, a, x)
+_mulxa(x::AbstractMatrix, a::TrackedArray) = Flux.Tracker.track(_mulxa, x, a)
 
-# @grad a::AbstractMatrix * b::AbstractVecOrMat =
-#   data(a)*data(b), Δ -> (Δ * transpose(b), transpose(a) * Δ)
+Flux.Tracker.@grad function _mulax(θ::TrackedArray, x)
+	return _mulax(Flux.data(θ), Flux.data(x)) , Δ -> (_∇mulax(Flux.data(θ), Flux.data(Δ), Flux.data(x)), _mulatx(Flux.data(θ), Flux.data(Δ)))
+end
+Flux.Tracker.@grad function _mulatx(θ::TrackedArray, x)
+  return _mulatx(Flux.data(θ), Flux.data(x)) , Δ -> (_∇mulax(Flux.data(θ), Flux.data(Δ), Flux.data(x)), (println("here");_mulax(Flux.data(θ), Flux.data(Δ))))
+end
+# (Δ * transpose(b), transpose(a) * Δ)
 
-# UnitaryMatrix{T} where {T<: TrackedArray}
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,33 +1,49 @@
 using Unitary, Test, LinearAlgebra, Flux
-using Unitary: UnitaryMatrix, ∇mul!
+using Unitary: UnitaryMatrix, _∇mulax, _mulax, _mulatx
+using Flux.Tracker: Params
 
 @testset "Testing multiplication and transposed" begin
 	a = UnitaryMatrix([1])
 	ad = Matrix(a)
 
-	x = rand(2,10);
-	@test a * x ≈ ad * x
-	@test transpose(a) * x ≈ transpose(ad) * x
-	@test transpose(x) * transpose(a) ≈ transpose(x) * transpose(a)
-	@test transpose(x) * a ≈ transpose(x) * ad
+	# for x in [randn(10),rand(2,10)]
+	x = rand(2,10)
+		@test a * x ≈ ad * x
+		@test transpose(a) * x ≈ transpose(ad) * x
+		@test transpose(x) * transpose(a) ≈ transpose(x) * transpose(ad)
+		@test transpose(x) * a ≈ transpose(x) * ad
+	# end
 
-	x = rand(10, 2);
-	@test x * a ≈ x * ad
-	@test x * transpose(a) ≈ x * transpose(ad)
-	@test transpose(a) * transpose(x) ≈ transpose(ad) * transpose(x)
-	@test a * transpose(x) ≈ ad * transpose(x)
+	# for x in [rand(1,2), rand(10, 2)];
+	x = rand(10,  2)
+		@test x * a ≈ x * ad
+		@test x * transpose(a) ≈ x * transpose(ad)
+		@test transpose(a) * transpose(x) ≈ transpose(ad) * transpose(x)
+		@test a * transpose(x) ≈ ad * transpose(x)
+	# end
 end
 
 
 @testset "Integration with Flux" begin
-	a = UnitaryMatrix(param([1]))
+	θ = [1.0]
+	a = UnitaryMatrix(param(θ))
+	x = param(rand(2,10))
+	ps = Params(params(a))
+	push!(ps, x)
 
-	x = rand(2,10)
+	grads = Flux.Tracker.gradient(() -> sum(sin.(a * x)), ps)
+	∇θ = Flux.data(grads[a.θ])
+	∇x = Flux.data(grads[x])
+
+	a, x = Flux.data(a), Flux.data(x)
+	isapprox(∇x, Flux.Tracker.ngradient(x -> sum(sin.(a * x)), x)[1], atol = 1e-6)
+	isapprox(∇θ, Flux.Tracker.ngradient(θ -> sum(sin.(_mulax(θ, x))), θ)[1], atol = 1e-6)
+
+	gradtest(a -> a*x, a)
+	isapprox(Flux.Tracker.ngradient(θ -> sum(Unitary._mulatx(x, θ)),θ)[1], _∇mulax(θ, ones(size(x)), x), atol = 1e-6)
+
 	@test a * x ≈ ad * x
 	x = rand(10, 2)
 	@test x * a ≈ x * ad
 end
 
-
- Flux.Tracker.ngradient(θ -> sum(Unitary._mulax!(similar(x), x, sin(θ[1]), cos(θ[1]))),[1.0])
- ∇mul!(a, Δ, x)
