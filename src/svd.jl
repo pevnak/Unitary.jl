3ad05e6b926c768773cffb7ecfc290ffe51775e0
Pevnak <pevnak@gmail.com>
2019-07-03 14:53:53 +0200

    it seems that I have forget to add file with svd layers

---
+++ src/svd.jl
@@ -0,0 +1,53 @@
+struct SVDDense{U, D, V, B, S}
+	u::U
+	d::D
+	v::V
+	b::B
+	σ::S
+end
+
+Flux.@treelike(SVDDense)
+
+SVDDense(σ) = SVDDense(UnitaryMatrix(param(rand(1))), 
+			param(rand(2)),
+			UnitaryMatrix(param(rand(1))),
+			param(rand(2)),
+			σ)
+
+
+(m::SVDDense)(x::AbstractMatVec) = m.σ.(m.u * (m.d .* (m.v * x)) .+ m.b)
+function (m::SVDDense)(x::Tuple)
+	x, logdet = x
+	pre = m.u * (m.d .* (m.v * x)) .+ m.b
+	g = Flux.Tracker.gradient(z -> sum(m.σ.(z)), pre, nest = true)[1]
+	(m.σ.(pre), logdet .+ sum(log.(g), dims = 1) .+ sum(log.(abs.(m.d) .+ 1f-6)))
+end
+
+struct InvertedSVDDense{U, D, V, B, S}
+	u::U
+	d::D
+	v::V
+	b::B
+	σ::S
+end
+Flux.@treelike(InvertedSVDDense)
+
+Base.inv(m::SVDDense) = InvertedSVDDense(inv(m.u), m.d, inv(m.v), m.b, inv(m.σ))
+Base.inv(m::InvertedSVDDense) = SVDDense(inv(m.u), m.d, inv(m.v), m.b, inv(m.σ))
+
+(m::InvertedSVDDense)(x::AbstractMatVec)  = m.v * ((m.u * (m.σ.(x) .- m.b)) ./ m.d)
+
+#define inversions of the most common functions
+function invselu(x::Real)
+  λ = NNlib.oftype(x/1, 1.0507009873554804934193349852946)
+  α = NNlib.oftype(x/1, 1.6732632423543772848170429916717)
+  x = x / λ
+  ifelse(x > 0, x/1, log.(1 + x/α))
+end
+
+Base.inv(::typeof(identity)) = identity
+Base.inv(::typeof(NNlib.selu)) = invselu
+Base.inv(::typeof(invselu)) = NNlib.selu
+
+#define inversion of a Chain
+Base.inv(m::Chain) = Chain(inv.(m.layers[end:-1:1])...)
--- test/sixgaussians.jl
+++ test/sixgaussians.jl
@@ -1,21 +1,43 @@
-using Unitary, Test, LinearAlgebra, Flux
+using Unitary, Test, LinearAlgebra, Flux, Statistics
 using Unitary: SVDDense
 using Flux.Tracker: Params, gradient
+using Flux.Optimise: train!
+using Plots
+plotly()
+
 
 function six_gaussians(l, ρ = 8)
 	x = reduce(hcat,[randn(2,l) .+ ρ .* [cos(ϕ), sin(ϕ)] for ϕ in 0:45:360])
-	x = x[:,randperm(size(x,2))]
+	# x = x[:,randperm(size(x,2))]
 	x = Float32.(x)
 end
 
-x = six_gaussians(100);
-xtst = six_gaussians(100)
+function single_gaussians(l, c =1)
+	Σ = [1.0 c; c 10.0]
+	Float32.(cholesky(Σ).U * randn(2, l))
+end
+
+
+x = single_gaussians(100);
+xtst = single_gaussians(100)
 
 log_normal(x) = - sum(x.^2, dims=1) / 2 .- size(x,1)*log(2π) / 2
 
-m = Chain(SVDDense(selu), SVDDense(selu), SVDDense(identity))
 function lkl(m, x)
 	x, l = m((x,0))
 	log_normal(x) .+ l
 end
 
+m = Chain(SVDDense(selu), SVDDense(selu), SVDDense(selu), SVDDense(selu), SVDDense(selu), SVDDense(selu),SVDDense(selu), SVDDense(selu), SVDDense(selu), SVDDense(selu), SVDDense(selu), SVDDense(selu), SVDDense(identity))
+opt = ADAM()
+ps = params(m)
+Flux.train!(x -> -mean(lkl(m, x)), ps, repeatedly(() -> (x,), 1000), opt; cb = () -> @show mean(lkl(m,x)) )
+
+o = Flux.data(m(x))
+scatter(o[1,:], o[2,:], title = "projected to latent")
+scatter(x[1,:], x[2,:], title = "original data")
+z = randn(2,1000)
+c = 1
+zz = z[:, maximum(abs.(z), dims = 1)[:] .< c]
+o = Flux.data(inv(m)(zz))
+scatter(o[1,:], o[2,:], title = "generated data")
