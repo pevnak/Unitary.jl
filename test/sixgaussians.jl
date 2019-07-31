5d80136c2551f15641f9160cc964942c8aaacc49
Pevnak <pevnak@gmail.com>
2019-07-31 20:16:21 +0200

    commit before branch

--- test/sixgaussians.jl
+++ test/sixgaussians.jl
@@ -1,4 +1,4 @@
-using Unitary, Test, LinearAlgebra, Flux, Statistics
+using Unitary, Test, LinearAlgebra, Flux, Statistics, IterTools
 using Unitary: SVDDense
 using Flux.Tracker: Params, gradient
 using Flux.Optimise: train!
@@ -12,14 +12,14 @@ function six_gaussians(l, ρ = 8)
 	x = Float32.(x)
 end
 
-function single_gaussians(l, c =1)
-	Σ = [1.0 c; c 10.0]
-	Float32.(cholesky(Σ).U * randn(2, l))
+function single_gaussians(l, ϵ)
+	Σ = [1.0 1.0; 1.0 1.0 + ϵ]
+	Σ * randn(2,l)
 end
 
 
-x = single_gaussians(100);
-xtst = single_gaussians(100)
+x = single_gaussians(100, 0.1);
+xtst = single_gaussians(100, 0.1)
 
 log_normal(x) = - sum(x.^2, dims=1) / 2 .- size(x,1)*log(2π) / 2
 
@@ -28,14 +28,18 @@ function lkl(m, x)
 	log_normal(x) .+ l
 end
 
-m = Chain(SVDDense(selu), SVDDense(selu), SVDDense(selu), SVDDense(selu), SVDDense(selu), SVDDense(selu),SVDDense(selu), SVDDense(selu), SVDDense(selu), SVDDense(selu), SVDDense(selu), SVDDense(selu), SVDDense(identity))
+m = SVDDense(identity)
 opt = ADAM()
 ps = params(m)
-Flux.train!(x -> -mean(lkl(m, x)), ps, repeatedly(() -> (x,), 1000), opt; cb = () -> @show mean(lkl(m,x)) )
+for i in 1:1000
+	Flux.train!(x -> -mean(lkl(m, x)), ps, repeatedly(() -> (x,), 1000), opt)
+	@show mean(lkl(m,x))
+end
 
-o = Flux.data(m(x))
-scatter(o[1,:], o[2,:], title = "projected to latent")
-scatter(x[1,:], x[2,:], title = "original data")
+mi = inv(m);
+o = Flux.data(mi(x))
+scatter(o[1,:], o[2,:], label = "sampled");
+scatter!(x[1,:], x[2,:], title = "original")
 z = randn(2,1000)
 c = 1
 zz = z[:, maximum(abs.(z), dims = 1)[:] .< c]
