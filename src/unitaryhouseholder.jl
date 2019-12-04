2611b26dc97524443af9a5f51d6e3614bf6267dd
Pevnak <pevnak@gmail.com>
2019-12-04 16:55:22 +0100

    a POC of a unitary matrix supporting setindex

--- src/unitaryhouseholder.jl
+++ src/unitaryhouseholder.jl
@@ -1,3 +1,59 @@
+struct UHT{F} <: AbstractMatrix{F}
+	Y::LowerTriangular{F, Matrix{F}}
+	T::Matrix{F}
+end
+
+
+Base.size(a::UHT) = size(a.Y)
+Base.size(a::UHT, i) = size(a.Y, i)
+
+Base.show(io, a::UHT) = pritnln(io, "Unitary matrix of size $(size(a.Y))")
+
+function UHT(x::Matrix)
+	@assert size(x,1) == size(x,2)
+	a = UHT(LowerTriangular(x), similar(x))
+	updatet!(a)
+	a
+end
+
+
+Base.getindex(a::UHT{F}, i, j) where {F} = (i < j) ? zero(F) : a.Y[i,j]
+
+function Base.copyto!(a::UHT, b::Matrix)
+	for i in 1:size(a.Y, 1)
+		for j in 1:size(a.Y, 2)
+			i < j && continue
+			a.Y[i,j] = b[i, j] 
+		end
+	end
+	updatet!(a)
+end
+
+function Base.copyto!(a::UHT, bc::Base.Broadcast.Broadcasted)
+	is, js = axes(bc)
+	for j in js 
+		for i in is 
+			i < j && continue
+			a.Y[i,j] = bc[i,j]
+		end
+	end
+	updatet!(a)
+end
+
+function updatet!(a::UHT)
+	println("updatet!")
+	Y = a.Y 
+	T = a.T
+	n = size(Y, 1)
+	@assert size(Y, 2) <= n
+	T .= 0
+	T[1, 1] = HH_t(Y, 1)
+	@inbounds for i = 2:n
+		T[i, i] = HH_t(Y, i)
+		T[1:i-1, i] = -T[i, i] * T[1:i-1, 1:i-1] * Y[:, 1:i-1]' * Y[:, i]
+	end
+end
+
 struct UnitaryHouseholder{M<:AbstractMatrix}
 	Y::M
 	transposed::Bool
