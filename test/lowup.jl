3c95e80dcc5fc5bd2e647734e92ce9b45ec9668f
tlauli <rivierak@gmail.com>
2020-03-07 16:00:39 +0100

    Add stronger tests

--- test/lowup.jl
+++ test/lowup.jl
@@ -7,35 +7,39 @@ using Zygote: gradient
 @testset "Test gradient functions" begin
 	cfdm = central_fdm(5, 1)
 	m = rand(5, 5)
-	x = rand(5, 5)
+	x = rand(5, 10)
 	Δ = ones(size(m*x))
 	ax = x
-	xa = x
+	xt = rand(10, 5)
+	Δt = ones(size(xt*m))
+	xa = xt
 	@test grad(cfdm, m -> sum(lowup(m) * x), m)[1] ≈
 	∇mulax(Δ, m, ax)[1]
 	@test grad(cfdm, x -> sum(lowup(m) * x), x)[1] ≈
 	∇mulax(Δ, m, ax)[2]
-	@test grad(cfdm, m -> sum(x*lowup(m)), m)[1] ≈
-	∇mulxa(Δ, m, xa)[1]
-	@test grad(cfdm, x -> sum(x*lowup(m)), x)[1] ≈
-	∇mulxa(Δ, m, xa)[2]
+	@test grad(cfdm, m -> sum(xt*lowup(m)), m)[1] ≈
+	∇mulxa(Δt, m, xa)[1]
+	@test grad(cfdm, x -> sum(xt*lowup(m)), xt)[1] ≈
+	∇mulxa(Δt, m, xa)[2]
 end
 
 @testset "Test gradient functions using inversions" begin
 	cfdm = central_fdm(5, 1)
 	m = rand(5, 5)
-	x = rand(5, 5)
+	x = rand(5, 10)
 	Δ = ones(size(m*x))
 	ax = lowup(m)*x
-	xa = x*lowup(m)
+	xt = rand(10, 5)
+	Δt = ones(size(xt*m))
+	xa = xt*lowup(m)
 	@test grad(cfdm, m -> sum(lowup(m) * x), m)[1] ≈
 	∇mulax_inv(Δ, m, ax)[1]
 	@test grad(cfdm, x -> sum(lowup(m) * x), x)[1] ≈
 	∇mulax_inv(Δ, m, ax)[2]
-	@test grad(cfdm, m -> sum(x*lowup(m)), m)[1] ≈
-	∇mulxa_inv(Δ, m, xa)[1]
-	@test grad(cfdm, x -> sum(x*lowup(m)), x)[1] ≈
-	∇mulxa_inv(Δ, m, xa)[2]
+	@test grad(cfdm, m -> sum(xt*lowup(m)), m)[1] ≈
+	∇mulxa_inv(Δt, m, xa)[1]
+	@test grad(cfdm, x -> sum(xt*lowup(m)), xt)[1] ≈
+	∇mulxa_inv(Δt, m, xa)[2]
 end
 
 @testset "Testing integration with Flux" begin
